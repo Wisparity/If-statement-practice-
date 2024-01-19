426b00c9555ddc15159cdcc42361f2220e486c00
Wisp <146360282+Wisparity@users.noreply.github.com>
2024-01-19 09:59:23 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+Choice1 = input ("Which is your favorite season?(Summer, Winter)")
+Choice2 = input ("Do you like sports?(Y/N)")
+if Choice1 == "Summer":
+    if Choice2 == "Y":
+        print("oooo, we should go swimming! C'mon, lets go get swimsuits!:D")
+    elif Choice2 == "N":
+        print("Aww, That's too bad, Maybe we could still go to the beach")
+elif Choice1 == "Winter":
+    if Choice2 == "Y":
+        print("Have you been snowboarding before? Oh dude you we HAVE to go! I know a really good gear spot")
+    elif Choice2 == "N":
+        print("It's all good, not all of us are the sporty type! We can hang out in that cafe nearby")
